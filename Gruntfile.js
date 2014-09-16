ca6198ea2648e6113fa333a3ccf2cc069b3158c1
Laurens Van Houtven <_@lvh.io>
2014-09-16 17:11:40 +0200

    No seriously fuck tabs

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,12 +7,12 @@ module.exports = function(grunt) {
     meta: {
       banner:
       '/*!\n' +
-	' * reveal.js <%= pkg.version %> (<%= grunt.template.today("yyyy-mm-dd, HH:MM") %>)\n' +
-	' * http://lab.hakim.se/reveal-js\n' +
-	' * MIT licensed\n' +
-	' *\n' +
-	' * Copyright (C) 2014 Hakim El Hattab, http://hakim.se\n' +
-	' */'
+        ' * reveal.js <%= pkg.version %> (<%= grunt.template.today("yyyy-mm-dd, HH:MM") %>)\n' +
+        ' * http://lab.hakim.se/reveal-js\n' +
+        ' * MIT licensed\n' +
+        ' *\n' +
+        ' * Copyright (C) 2014 Hakim El Hattab, http://hakim.se\n' +
+        ' */'
     },
 
     qunit: {
@@ -21,83 +21,83 @@ module.exports = function(grunt) {
 
     uglify: {
       options: {
-	banner: '<%= meta.banner %>\n'
+        banner: '<%= meta.banner %>\n'
       },
       build: {
-	src: 'js/reveal.js',
-	dest: 'js/reveal.min.js'
+        src: 'js/reveal.js',
+        dest: 'js/reveal.min.js'
       }
     },
 
     cssmin: {
       compress: {
-	files: {
-	  'css/reveal.min.css': [ 'css/reveal.css' ]
-	}
+        files: {
+          'css/reveal.min.css': [ 'css/reveal.css' ]
+        }
       }
     },
 
     sass: {
       main: {
-	files: {
-	  'css/theme/default.css': 'css/theme/source/default.scss',
-	  'css/theme/beige.css': 'css/theme/source/beige.scss',
-	  'css/theme/night.css': 'css/theme/source/night.scss',
-	  'css/theme/serif.css': 'css/theme/source/serif.scss',
-	  'css/theme/simple.css': 'css/theme/source/simple.scss',
-	  'css/theme/sky.css': 'css/theme/source/sky.scss',
-	  'css/theme/moon.css': 'css/theme/source/moon.scss',
-	  'css/theme/solarized.css': 'css/theme/source/solarized.scss',
-	  'css/theme/blood.css': 'css/theme/source/blood.scss',
+        files: {
+          'css/theme/default.css': 'css/theme/source/default.scss',
+          'css/theme/beige.css': 'css/theme/source/beige.scss',
+          'css/theme/night.css': 'css/theme/source/night.scss',
+          'css/theme/serif.css': 'css/theme/source/serif.scss',
+          'css/theme/simple.css': 'css/theme/source/simple.scss',
+          'css/theme/sky.css': 'css/theme/source/sky.scss',
+          'css/theme/moon.css': 'css/theme/source/moon.scss',
+          'css/theme/solarized.css': 'css/theme/source/solarized.scss',
+          'css/theme/blood.css': 'css/theme/source/blood.scss',
           'css/theme/lvh.css': 'css/theme/source/lvh.scss'
-	}
+        }
       }
     },
 
     jshint: {
       options: {
-	curly: false,
-	eqeqeq: true,
-	immed: true,
-	latedef: true,
-	newcap: true,
-	noarg: true,
-	sub: true,
-	undef: true,
-	eqnull: true,
-	browser: true,
-	expr: true,
-	globals: {
-	  head: false,
-	  module: false,
-	  console: false,
-	  unescape: false,
-	  define: false,
-	  exports: false
-	}
+        curly: false,
+        eqeqeq: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: true,
+        eqnull: true,
+        browser: true,
+        expr: true,
+        globals: {
+          head: false,
+          module: false,
+          console: false,
+          unescape: false,
+          define: false,
+          exports: false
+        }
       },
       files: [ 'Gruntfile.js', 'js/reveal.js' ]
     },
 
     connect: {
       server: {
-	options: {
-	  port: port,
-	  base: '.',
+        options: {
+          port: port,
+          base: '.',
           livereload: true,
           open: true
-	}
+        }
       }
     },
 
     zip: {
       'reveal-js-presentation.zip': [
-	'index.html',
-	'css/**',
-	'js/**',
-	'lib/**',
-	'images/**',
-	'plugin/**'
+        'index.html',
+        'css/**',
+        'js/**',
+        'lib/**',
+        'images/**',
+        'plugin/**'
       ]
     },
 
@@ -106,12 +106,12 @@ module.exports = function(grunt) {
         livereload: true
       },
       main: {
-	files: [ 'Gruntfile.js', 'js/reveal.js', 'css/reveal.css' ],
-	tasks: 'default'
+        files: [ 'Gruntfile.js', 'js/reveal.js', 'css/reveal.css' ],
+        tasks: 'default'
       },
       theme: {
-	files: [ 'css/theme/source/*.scss', 'css/theme/template/*.scss' ],
-	tasks: 'themes'
+        files: [ 'css/theme/source/*.scss', 'css/theme/template/*.scss' ],
+        tasks: 'themes'
       },
       html: {
         files: [ 'index.html']
